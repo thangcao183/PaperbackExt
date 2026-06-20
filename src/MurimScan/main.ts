d29ea2b2a4c406d10987ada2b9c2c0b585d145e1
Nicartjay <Nicartjay@users.noreply.github.com>
2026-06-20 23:02:41 +0800

    Add MurimScan source via shared ZeistManga template

---
+++ src/MurimScan/main.ts
@@ -0,0 +1,17 @@
+import { ContentRating } from "@paperback/types";
+import { ZeistMangaExtension } from "../utils/zeistmanga/template";
+
+export const MurimScan = new ZeistMangaExtension({
+  name: "MurimScan",
+  baseUrl: "https://www.murimscans.site",
+  popularMangaSelector: ".PopularPosts article",
+  popularMangaSelectorTitle: ".post-title a",
+  mangaDetailsSelector: "main",
+  mangaDetailsSelectorGenres: "dl.flex:contains(Genre) a[rel=tag], dl.flex:contains(Type) a[rel=tag]",
+  mangaDetailsSelectorInfo: "dl.flex",
+  mangaDetailsSelectorInfoTitle: "dt",
+  mangaDetailsSelectorInfoDescription: "dd",
+  pageListSelector: ".post-body, .check-box",
+  contentRating: ContentRating.MATURE,
+  langCode: "🇬🇧",
+});
